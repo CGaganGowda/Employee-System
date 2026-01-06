b62aafc768e5b4ab97c8ca693146cb42d2f8296c
CGaganGowda <95483586+CGaganGowda@users.noreply.github.com>
2026-01-06 21:48:44 +0530

    Add files via upload

---
+++ Employee/src/test/java/com/system/Employee/controller/EmployeeController.java
@@ -0,0 +1,74 @@
+package com.manage.employee.controller;
+
+import com.manage.employee.dto.EmployeeDto;
+import com.manage.employee.exception.ErrorDetails;
+import com.manage.employee.exception.ResourceNotFoundException;
+import com.manage.employee.model.Employee;
+import com.manage.employee.service.EmployeeService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+@RequestMapping("api/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("create")
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody @Valid EmployeeDto employee){
+        //System.out.println("createEmployee "+employee);
+        EmployeeDto createdEmployee = employeeService.createEmployee(employee);
+        return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("get")
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
+        List<EmployeeDto> employeeList = employeeService.getAllEmployees();
+        return new ResponseEntity<>(employeeList, HttpStatus.OK);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<EmployeeDto> getEmployee(@PathVariable Long id){
+        EmployeeDto employee = employeeService.getEmployee(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<EmployeeDto> updateEmployee(@RequestBody @Valid EmployeeDto employee,@PathVariable Long id){
+        employee.setId(id);
+        EmployeeDto updatedEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee deleted with id: "+id, HttpStatus.OK);
+    }
+
+    @DeleteMapping("deleteAll")
+    public ResponseEntity<String> deleteAllEmployees(){
+        employeeService.deleteAllEmployees();
+        return new ResponseEntity<>("All employees have been deleted", HttpStatus.OK);
+    }
+
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
+//        ErrorDetails errorDetails = new ErrorDetails(
+//                LocalDateTime.now(),
+//                exception.getMessage(),
+//                webRequest.getDescription(true),
+//                "USER_NOT_FOUND"
+//        );
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
+
+}
